be4540467cafbb6905e9a89ae71eaf825cbb5da9
alexquar <alexanderquarrie@gmail.com>
2024-03-31 18:06:59 -0400

    login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,33 @@
-import './Login.css'
-
+import styles from './Login.module.css'
+import { useState } from 'react'
 export default function Login() {
-  return (
-    <div>
-      Login
-    </div>
-  )
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const handleSubmit = (e) => {
+    e.preventDefault()
+    //next
 }
+
+    return (
+        <form onSubmit={handleSubmit} className={styles['login-form']}>
+          <h2>login</h2>
+          <label>
+            <span>email:</span>
+            <input 
+              type="email" 
+              onChange={(e) => setEmail(e.target.value)} 
+              value={email}
+            />
+          </label>
+          <label>
+            <span>password:</span>
+            <input 
+              type="password" 
+              onChange={(e) => setPassword(e.target.value)} 
+              value={password} 
+            />
+          </label>
+          <button className="btn">Login</button>
+        </form>
+      )
+    }
