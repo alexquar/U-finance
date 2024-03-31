63b020c6d976a84948ff14bb7188744a8eb4d2d0
alexquar <alexanderquarrie@gmail.com>
2024-03-31 17:59:10 -0400

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import './Navbar.css'
+import { Link } from "react-router-dom"
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div>
-      Nav
-    </div>
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>U Finance</li>
+        
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
   )
 }
