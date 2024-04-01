3be1330f63be5f7702b8bde8f3de6a48a23a1d0f
alexquar <alexanderquarrie@gmail.com>
2024-04-01 10:23:09 -0400

    logout button and use

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom"
 import styles from './Navbar.module.css'
-
+import { useLogout } from "../hooks/useLogout"
 export default function Navbar() {
+  const {logout} = useLogout()
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -9,6 +10,8 @@ export default function Navbar() {
         
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+
+        <li> <button onClick={logout} className="btn"> Logout </button></li>
       </ul>
     </nav>
   )
