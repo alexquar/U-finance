faf8f91b9d11af6f5e26345c2162386326a41a4f
alexquar <alexanderquarrie@gmail.com>
2024-04-01 13:29:15 -0400

    route guards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 import Navbar from './components/Navbar';
 import { useAuthContext } from './hooks/useAuthContext';
 function App() {
-  const {authIsReady} = useAuthContext()
+  const {authIsReady, user} = useAuthContext()
   
   return (
     <div className="App">
     {authIsReady &&  <BrowserRouter>
      <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+       {user && <Route path="/" element={<Home />} /> }
+       {!user && <Route path="/" element={<Navigate to="/login" />} /> }
+        {!user && <Route path="/login" element={<Login />} />}
+        {user && <Route path="/login" element={<Navigate to="/" />} />}
+        {!user && <Route path="/signup" element={<Signup />} />}
+        {user && <Route path="/signup" element={<Navigate to="/" />} />}
     </Routes>
         
       </BrowserRouter>
