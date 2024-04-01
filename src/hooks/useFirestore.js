89426d9734cd27af15670c92c0632951706b9ffc
alexquar <alexanderquarrie@gmail.com>
2024-04-01 15:01:08 -0400

    adding docs

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -10,7 +10,12 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
-    
+    case "IS_PENDING":
+      return {success: false, isPending: true, error: null, document: null}
+    case "ERROR":
+      return {success: false, isPending: false, error: action.payload, document: null}
+    case "ADDED_DOCUMENT":
+      return {success: true, isPending: false, error: null, document: action.payload}
     default:
       return state
   }
@@ -20,16 +25,30 @@ export const useFirestore = (collection) => {
   const [response, dispatch] = useReducer(firestoreReducer, initialState)
   const [isCancelled, setIsCancelled] = useState(false)
 
-  // collection ref
   const ref = projectFirestore.collection(collection)
+
+
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action)
+    }
+  }
   
-  // add a document
   const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" })
+
+    try {
+      const addedDocument = await ref.add({...doc})
+      dispatchIfNotCancelled({ type: "ADDED_DOCUMENT", payload: addedDocument })
+    }
+    catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message })
+    }
 
   }
 
   // delete a document
-  const deleteDocument = async (id) => {
+  const deleteDocument = async (doc) => {
 
   }
 
